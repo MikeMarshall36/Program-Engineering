eddb82500da2abbfaaf5955a6f67b111526e0ff6
Сергей Витальевич Королев <90028680+MikeMarshall36@users.noreply.github.com>
2023-02-14 13:34:17 +0300

    some changes
    # Были добавлены комментарии
    # Теперь каждая конвертация происходит в отдельном потоке
    # Исправлены минорные баги

--- Сайт/application.py
+++ Сайт/application.py
@@ -1,81 +1,85 @@
-import time
-from main import pdf_to_audio
-from flask import *
-from flask_sqlalchemy import SQLAlchemy
+import time  # Импортируем модуль времени
+from pathlib import Path  # модуль для работы с путями, но нам нужен только инструмент для файлов
+from main import pdf_to_audio  # модуль для конвертации
+from flask import *  # модуль для создания веб-приложений
+from flask_sqlalchemy import SQLAlchemy  # пакет ORM СУБД
 
 
-application = Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////First.db'
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(application)
+application = Flask(__name__)  # инициализация экземпляра класса веб-приложения на котором будем собирать проект
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////First.db'  # инициализация базы данных в проекте
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # игнорируем не работающую часть пакета (она нам не нужна)
+db = SQLAlchemy(application)  # создаем экземпляр класса ORM
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(32), nullable=False)
-    e_mail = db.Column(db.String(64), nullable=False)
-    passwd = db.Column(db.String(32), nullable=False)
+class User(db.Model):  # таблица User базы данных
+    id = db.Column(db.Integer, primary_key=True)  # графа идентификатора
+    user_name = db.Column(db.String(32), nullable=False)  # графа с именем пользователя
+    e_mail = db.Column(db.String(64), nullable=False)  # поле данных почты
+    passwd = db.Column(db.String(32), nullable=False)  # поле пароля (позже будет храниться в зашифрованном виде)
 
-    def __init__(self, file_id, user_name, passwd, e_mail):
-        self.passwd = passwd.strip()
+    def __init__(self, file_id, user_name, passwd, e_mail):  # инициализация класса User (она же таблица, но у нас ORM)
+        self.passwd = passwd.strip()  # присваиваем соответствующие значения, при этом удаляем пробелы
         self.e_mail = e_mail.srtip()
         self.user_name = user_name.strip()
-        self.file_id = [File(file_id=file_id)]
+        self.file_id = [File(file_id=file_id)]  # делаем ссылку на вторую таблицу, где есть нужная нам строка
 
 
-class File(db.Model):
-    file_id = db.Column(db.Integer, primary_key=True)
-    file_name = db.Column(db.String(32), nullable=False)
-    file = db.Column(db.BLOB)
+class File(db.Model):  # таблица файла в бд
+    file_id = db.Column(db.Integer, primary_key=True)  # id
+    file_name = db.Column(db.String(32), nullable=False)  # имя файла
+    file = db.Column(db.BLOB)  # сам файл в типе blob (используется для хранения файлов в бд)
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # id пользователя-владельца
+    user_id = db.relationship("User", backref=db.backref('file_id', lazy=True))  # ссылка на другую таблицу
 
-    id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user_id = db.relationship("User", backref=db.backref('file_id', lazy=True))
 
-
-@application.route('/home', methods=['GET'])
+@application.route('/home', methods=['GET'])  # прописываем пути для достижения домашней страницы
 @application.route('/', methods=['GET'])
 @application.route('/main', methods=['GET'])
 @application.route('/main_page', methods=['GET'])
-def home():
-    return render_template('main.html')
+def home():  # функция для отлова запроса домашней страницы
+    return render_template('main.html')  # возвращаем главную страницу
 
 
-@application.route('/index', methods=['GET'])
-def index():
-    return render_template('index.html')
+@application.route('/index', methods=['GET'])  # тестовое оформление (на нее не попасть, если не знать о ней)
+def index():  # отлов запроса к странице индекса
+    return render_template('index.html')  # возвращаем страницу
 
 
-@application.route('/help', methods=['GET'])
-@application.route('/guide', methods=['GET'])
-@application.route('/instruction', methods=['GET'])
-def guide():
-    return render_template('guide.html')
+@application.route('/guide', methods=['GET'])  # путь для страницы с инструкцией пользователя
+@application.route('/instruction', methods=['GET'])  # альтернативный путь
+def guide():  # отлов путей
+    return render_template('guide.html')  # возврат страницы
 
 
-@application.route('/settings', methods=['GET'])
-def settings():
-    return render_template('settings.html')
+@application.route('/settings', methods=['GET'])  # путь к странице с настройками
+def settings():  # поиск обращений к определенному пути
+    return render_template('settings.html')  # возврат страницы с настройками
 
 
-@application.route('/about', methods=['GET'])
-def about():
-    return render_template('about.html')
+@application.route('/about', methods=['GET'])  # Страница о нас (но зачем?)
+def about():  # обработчик пути
+    return render_template('about.html')  # Возврат страницы (таков путь)
 
 
-@application.route('/uploader', methods=['GET', 'POST'])
-def uploader():
-    if request.method == 'POST':
-        file = request.files['file']
-        file.save(file.filename)
-        print(file.filename)
-        inputFile = (f'./{file.filename}')
-        pdf_to_audio(inputFile)
-        file_name = str(file.filename).split('.')
-        time.sleep(5)
-        return send_file(f'files/{file_name[0]}.mp3', as_attachment=True)
-    if request.method == 'GET':
-        return render_template('uploader.html')
+@application.route('/uploader', methods=['GET', 'POST'])  # Страница с конвертором
+def uploader():  # обработчик
+    if request.method == 'POST':  # Проверка на запрос с методом POST
+        file = request.files['file']  # получаем файл
+        file.save(file.filename)  # сохраняем полученный файл
+        print(f'[&]{file.filename}')  # вывод в консоль для отладки
+        inputFile_name = (f'./{file.filename}')  # Добавляем необходимые символы для работы конвертора
+        pdf_to_audio(inputFile_name)  # Производим конвертацию
+        file_name = Path(file.filename).stem  # Вырезаем имя файла
+        time.sleep(5)  # Ожидаем 5 секунд, на случай объемных файлов
+        return send_file(f'files/{file_name}.mp3', as_attachment=True)  # Возврат получившегося файла
+    if request.method == 'GET':  # Проверка запроса с методом GET
+        return render_template('uploader.html')  # возвращаем страницу конвертора
 
 
-if __name__ == '__main__':
-    application.run(debug=False)
+if __name__ == '__main__':  # Создаем точку доступа
+    application.run(debug=False)  # Запускаем приложение без опции дебага
+"""
+Ограничение по расширению файла прописаны в коде html странице, так что здесь их нет и не будет.
+К бд обращений пока нет, так как сама схема требует доработок и исправлений.
+В дальнейших версиях добавим бд и взаимодействие с ней, а пока она висит как заглушка. 
+"""
--- Сайт/main.py
+++ Сайт/main.py
@@ -1,61 +1,60 @@
-import threading
-import pyttsx3
-import pdfplumber
-from pathlib import Path
-import os
-import docx2txt
-import langid
+import pyttsx3  # модуль для озвучки текса (используя голоса операционной системы)
+import pdfplumber  # модуль для чтения pdf файлов
+from multiprocessing import Process  # модуль для создания отдельных процессов
+from pathlib import Path  # модуль для работы с путями в файловой системе
+import os  # модуль для взаимодействия с операционной системой
+import docx2txt  # модуль для чтения docx файла
+import langid  # модуль для определения языка
 
 
-def pdf_to_audio(file_path='/'):
-    threading.Thread(target=pdf_to_audio_processor(file_path)).start()
+def pdf_to_audio(file_path='/'):  # Функция для запуска конвертора в отдельном процессе
+    p = Process(target=pdf_to_audio_processor(file_path))  # инициализация процесса
+    p.start()  # запуск процесса
+    p.join()  # завершение процесса
 
 
-def pdf_to_audio_processor(file_path='./'):
-    if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
-        print(f'[!] {Path(file_path).stem} is processing...')
-        with pdfplumber.PDF(open(file=file_path, mode='rb')) as pdf:
-            pages = [pages.extract_text() for pages in pdf.pages]
-        doc_text = ''.join(pages).replace('\n', ' ')
-        print(doc_text)
-        lang = langid.classify(doc_text)
-        audio = pyttsx3.init()
-        print(lang)
+def pdf_to_audio_processor(file_path='./'):  # конвертор
+    if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':  # проверка на pdf файл
+        print(f'[!] {Path(file_path).stem} is processing...')  # маркер начала конвертации
+        with pdfplumber.PDF(open(file=file_path, mode='rb')) as pdf:  # чтение файла
+            pages = [pages.extract_text() for pages in pdf.pages]  # переносим все в одну строку
+        doc_text = ''.join(pages).replace('\n', '')
+        print(doc_text)  # вывод строки
+        lang = langid.classify(doc_text)  # определение языка текста
+        audio = pyttsx3.init()  # инициализация чтения
+        print(lang)  # вывод языка
 
-        ru = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_RU-RU_IRINA_11.0'
-        en = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'
+        ru = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_RU-RU_IRINA_11.0'  # читалка на русском
+        en = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'  # читалка на англ
 
-        if lang[0] == 'en':
-            audio.setProperty('voice', en)
+        if lang[0] == 'en':  # проверка языка текста
+            audio.setProperty('voice', en)  # используем соответствующий язык читалки
         if lang[0] == 'ru':
             audio.setProperty('voice', ru)
-        file_name = Path(file_path).stem
-        audio.save_to_file(doc_text, f'files/{file_name}.mp3')
-        audio.runAndWait()
-        os.remove(file_path)
-        return print(f'[+] {file_name} has been converted to audio!')
+        file_name = Path(file_path).stem  # получаем название файла
+        audio.save_to_file(doc_text, f'files/{file_name}.mp3')  # указываем куда сохранить данные
+        audio.runAndWait()  # запускаем читалку
+        os.remove(file_path)  # удаляем исходный файл
+        return print(f'[+] {file_name} has been converted to audio!')  # маркер готовности файла
 
-    elif Path(file_path).is_file() and Path(file_path).suffix == '.docx':
-        print(f'[!] {Path(file_path).stem} is processing...')
-        doc_text = str(docx2txt.process(file_path)).replace('\n', ' ')
-        print(doc_text)
-        lang = langid.classify(doc_text)
-        audio = pyttsx3.init()
-        ru = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_RU-RU_IRINA_11.0'
-        en = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'
-
-        if lang[0] == 'en':
-            audio.setProperty('voice', en)
-        if lang[0] == 'ru':
-            audio.setProperty('voice', ru)
-        file_name = Path(file_path).stem
-        audio.save_to_file(doc_text, f'files/{file_name}.mp3')
-        audio.runAndWait()
-        os.remove(file_path)
-        return print(f'[+] {file_name} has been converted to audio!')
-    else:
-        return print('[!] File not found')
+    elif Path(file_path).is_file() and Path(file_path).suffix == '.docx':  # проверка на docx файл
+        print(f'[!] {Path(file_path).stem} is processing...')  # маркер начала конвертации
+        doc_text = str(docx2txt.process(file_path)).replace('\n', '')  # переносим все в одну строку
+        print(doc_text)  # выводим текст
+        lang = langid.classify(doc_text)  # определяем язык текста
+        audio = pyttsx3.init()  # инициализируем читалку
+        audio.setProperty('voice', f'{lang[0]}')  # устанавливаем соответствующий язык
+        file_name = Path(file_path).stem  # получаем название исходного файла
+        print(file_name)  # выводим имя файла для отладки
+        audio.save_to_file(doc_text, f'files/{file_name}.mp3')  # указываем место хранения данных
+        audio.runAndWait()  # запускаем читалку
+        os.remove(file_path)  # удаляем исходный файл
+        return print(f'[+] {file_name} has been converted to audio!')  # Маркер завершения конвертации
+    else:  # если файл не был найден или не имеет поддерживаемого расширения
+        return print('[!] File not found')  # выводим в консоль сообщение
 
 
-if __name__ == "__main__":
-    pdf_to_audio('./CV.pdf')
+if __name__ == "__main__":  # создаем точку доступа
+    p = Process(target=pdf_to_audio_processor('./Возможные вопросы по ПИС.docx'))  # инициализируем процесс для проверки
+    p.start()  # запускаем процесс
+    p.join()  # завершаем процесс
